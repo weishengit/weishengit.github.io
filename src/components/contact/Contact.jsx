5ed0c2c1ed629189adb5e85f2575c2666c3aeaab
weishengit <weishenemail@gmail.com>
2022-03-23 02:44:52 +0800

    add buildpack

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { MdMailOutline } from 'react-icons/md';
 import { RiMessengerLine } from 'react-icons/ri';
 import { FaLinkedin } from 'react-icons/fa';
@@ -6,6 +7,16 @@ import './contact.css';
 
 
 const Contact = () => {
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+	
+		emailjs.sendForm('service_mknw9ps', 'template_m60r4is', form.current, '_iLN2KyXuDqxsSoCW')
+
+		e.target.reset();
+	};
+
 	return (
 		<section id='contact' aria-label='Contact'>
 			<h5>Get In Touch</h5>
@@ -14,23 +25,28 @@ const Contact = () => {
 			<div className="container contact__container">
 
 				<div className="contact__options">
-
 					<article className="contact__option">
-						<MdMailOutline />
+						<MdMailOutline className='contact__option-icon'/>
 						<h4>Email</h4>
 						<h5>julesdaryldiego@gmail.com</h5>
-						<a href="mailto:julesdaryldiego@gmail.com">Send an email</a>
+						<a 
+							href="mailto:julesdaryldiego@gmail.com"
+							target="_blank" 
+							rel='noreferrer'>Send an email</a>
 					</article>
 
 					<article className="contact__option">
-						<RiMessengerLine />
+						<RiMessengerLine className='contact__option-icon'/>
 						<h4>Messenger</h4>
 						<h5>Jules Diego</h5>
-						<a href="https://m.me/jules.diego">Send a message</a>
+						<a 
+							href="https://m.me/jules.diego"
+							target="_blank" 
+							rel='noreferrer'>Send a message</a>
 					</article>
 
 					<article className="contact__option">
-						<FaLinkedin />
+						<FaLinkedin className='contact__option-icon'/>
 						<h4>LinkedIn</h4>
 						<h5>Jules Diego</h5>
 						<a 
@@ -43,8 +59,14 @@ const Contact = () => {
 
 				</div>
 
-				<form action="">
-
+				<form 
+					ref={form}
+					onSubmit={sendEmail}
+				>
+					<input type="text" name='name' placeholder='Your Name' required	/>
+					<input type="email" name='email' placeholder='Your Email' required/>
+					<textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+					<button className="btn btn-primary" type='submit'>Send Message</button>
 				</form>
 
 			</div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { BsFacebook, BsGithub, BsLinkedin } from 'react-icons/bs';
+import './footer.css';
 
 const Footer = () => {
 	return (
-		<div>Footer</div>
+		<footer>
+			<a 
+				href="#header"
+				className='footer__logo'
+			>
+				Diego
+			</a>
+			<ul className="permalinks">
+				<li><a href="#header">Home</a></li>
+				<li><a href="#about">About</a></li>
+				<li><a href="#experience">Experience</a></li>
+				<li><a href="#services">Services</a></li>
+				<li><a href="#portfolio">Portfolio</a></li>
+				<li><a href="#testimonials">Testimonials</a></li>
+				<li><a href="#contact">Contact</a></li>
+			</ul>
+
+			<div className="footer__socials">
+				<a href="https://facebook.com">
+					<BsFacebook />
+				</a>
+				<a href="https://github.com">
+					<BsGithub />
+				</a>
+				<a href="https://linkedin.com">
+					<BsLinkedin />
+				</a>
+			</div>
+
+			<div className="footer__copyright">
+				<small>&copy; Diego. All rights reserved.</small>
+			</div>
+		</footer>
 	);
 };
 
